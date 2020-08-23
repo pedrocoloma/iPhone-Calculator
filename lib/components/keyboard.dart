9da699a83b719a228200372e9a75716fe77b7744
Pedro <pfcaraujo@uolinc.com>
2020-08-23 11:42:23 -0300

    Makes keyboard Stateless

--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 import 'button_row.dart';
 import 'button.dart';
 
-class Keyboard extends StatefulWidget {
+class Keyboard extends StatelessWidget {
   final void Function(String) callback;
   final String _selectedOperation;
 
   Keyboard(this.callback, this._selectedOperation);
 
-  @override
-  _KeyboardState createState() => _KeyboardState();
-}
-
-class _KeyboardState extends State<Keyboard> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,91 +17,91 @@ class _KeyboardState extends State<Keyboard> {
           ButtonRow([
             Button.lighter(
               text: 'C',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.lighter(
               text: '+/-',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.lighter(
               text: '%',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.operation(
               text: '/',
-              isSeleccted: '/' == widget._selectedOperation,
-              callback: widget.callback,
+              isSeleccted: '/' == _selectedOperation,
+              callback: callback,
             ),
           ]),
           ButtonRow([
             Button(
               text: '7',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '8',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '9',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.operation(
               text: 'x',
-              isSeleccted: 'x' == widget._selectedOperation,
-              callback: widget.callback,
+              isSeleccted: 'x' == _selectedOperation,
+              callback: callback,
             ),
           ]),
           ButtonRow([
             Button(
               text: '4',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '5',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '6',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.operation(
               text: '-',
-              isSeleccted: '-' == widget._selectedOperation,
-              callback: widget.callback,
+              isSeleccted: '-' == _selectedOperation,
+              callback: callback,
             ),
           ]),
           ButtonRow([
             Button(
               text: '1',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '2',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '3',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.operation(
               text: '+',
-              isSeleccted: '+' == widget._selectedOperation,
-              callback: widget.callback,
+              isSeleccted: '+' == _selectedOperation,
+              callback: callback,
             ),
           ]),
           ButtonRow([
             Button.large(
               text: '0',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button(
               text: '.',
-              callback: widget.callback,
+              callback: callback,
             ),
             Button.operation(
               text: '=',
-              callback: widget.callback,
+              callback: callback,
             ),
           ]),
           SizedBox(
